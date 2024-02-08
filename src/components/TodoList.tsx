7c7ddcd2c08e7844bb877ccf04f2e24d2e38e7f8
Alex Reinoso <143521882+areinobay@users.noreply.github.com>
2024-02-08 00:08:55 -0500

    todo list react basic types

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,74 @@
+// File: src/TodoList.tsx
+
+import React, { useState } from 'react';
+
+interface Todo {
+  id: number;
+  task: string;
+  completed: boolean;
+}
+
+const TodoList: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTask, setNewTask] = useState<string>('');
+
+  const addTodo = (): void => {
+    if (newTask.trim() !== '') {
+      const newTodo: Todo = {
+        id: Date.now(),
+        task: newTask,
+        completed: false,
+      };
+
+      setTodos([...todos, newTodo]);
+      setNewTask('');
+    }
+  };
+
+  const toggleTodo = (id: number): void => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id: number): void => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h2>Your Todo List</h2>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodo(todo.id)}
+            />
+            <span
+              style={{
+                textDecoration: todo.completed ? 'line-through' : 'none',
+              }}
+            >
+              {todo.task}
+            </span>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
